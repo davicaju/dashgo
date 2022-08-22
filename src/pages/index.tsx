a9fd1e010fd02b3ccdc6fba5f147637f4f4546e9
Davi Caju <devcaju@gmail.com>
2022-08-22 19:37:30 -0300

    feat: add input on header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,27 @@
-export default function Home() {
-  return <h1>Dashgo</h1>;
+import { Flex, Button, Stack } from "@chakra-ui/react";
+
+import { Input } from "../components/Form/Input";
+
+export default function SignIn() {
+  return (
+    <Flex width="100vw" height="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        padding="8"
+        borderRadius={8}
+        flexDirection="column"
+      >
+        <Stack spacing={4}>
+          <Input name="email" label="E-mail" type="email" />
+          <Input name="password" label="Password" type="password" />
+        </Stack>
+        <Button type="submit" marginTop="6" colorScheme="pink" size="lg">
+          Sign-in
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
